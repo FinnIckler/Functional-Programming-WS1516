cc6c3b77405a4bbe4efd8d6871a3cc6d4d11c77c
Justin Humm <justin.humm@posteo.de>
2017-06-01 13:20:10 +0200

    add dep

--- default.nix
+++ default.nix
@@ -1,6 +1,12 @@
 with import <nixpkgs> {};
 #{pkgs, stdenv, buildEnv, ...}:
 
+let
+  hs = pkgs.fetchurl {
+    url = "https://ipfs.io/ipfs/QmRov5YDoer4dd75haUaVRRxgQh6y2h3QzRuEDvfnZUn3R/hs.tar.gz";
+    sha256 = "00acb13zgffy5mrq5cmwm7k8vqjm4678f8gxzm96mvmy0229iwcb";
+  };
+in
 stdenv.mkDerivation {
   name = "FP-script";
 
@@ -13,6 +19,7 @@ stdenv.mkDerivation {
   ];
 
   buildPhase = ''
+    tar -xf ${hs}
     xelatex -shell-escape Mitschrieb.tex
   '';
 
